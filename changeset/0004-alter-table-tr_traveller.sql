682574482b650dd3d5574c1c08d9a03d602ce8fa
devi <utilities@evimay.mailer.me>
2025-07-05 16:31:36 +1000

    fix: add rollback query

--- changeset/0004-alter-table-tr_traveller.sql
+++ changeset/0004-alter-table-tr_traveller.sql
@@ -1,2 +1,5 @@
 ALTER TABLE public.tr_traveller ADD COLUMN release_date date NULL;
 ALTER TABLE public.tr_traveller ADD COLUMN job varchar(20) NULL;
+
+--rollback ALTER TABLE public.tr_traveller DROP COLUMN release_date;
+--rollback ALTER TABLE public.tr_traveller DROP COLUMN job;
