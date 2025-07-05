fa66ca62f74a85449fa224f9c1c435957d047809
devi <utilities@evimay.mailer.me>
2025-07-05 16:41:35 +1000

    fix: add liquibase sql format & changeset label

--- changeset/0004-alter-table-tr_traveller.sql
+++ changeset/0004-alter-table-tr_traveller.sql
@@ -1,3 +1,10 @@
+--liquibase formatted sql
+
+--changeset devi:0004 label:v1.0.2 
+
+--comment: postgresql
+
+
 ALTER TABLE public.tr_traveller ADD COLUMN release_date date NULL;
 ALTER TABLE public.tr_traveller ADD COLUMN job varchar(20) NULL;
 
